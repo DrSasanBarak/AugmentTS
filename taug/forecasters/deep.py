d72f586067d42fa15cf541cf12ed42ec7641eca3
Amirabbas Asadi <amir137825@gmail.com>
2021-11-14 22:19:20 +0330

    making the default forecaster customizable

--- taug/forecasters/deep.py
+++ taug/forecasters/deep.py
@@ -19,49 +19,61 @@ class DeepForecaster():
     """
     Base class for deep forecasters
     """
-    def __init__(self, *args, **kwargs):
-        self.forecaster = None
+    def __init__(self, window_size, steps_ahead, loss='mae', optimizer='adam', metrics=None):
+        
+        self.steps_ahead = steps_ahead
+        self.window_size = window_size
+        self.loss = loss
+        self.optimizer = optimizer
+        
+        if metrics is None:
+            self.metrics = ['mae', 'mse']
+        
+        self.construct_forecaster()
+        
+        self.fit = self.forecaster.fit
+        self.predict = self.forecaster.predict
         
     
-    def construct_forecaster(self, *args, **kwargs):
-        raise NotImplementedError("The forecaster is not implemented. You must provide an implementation for construct_forecaster method")
-        
+    def construct_forecaster(self):
+        final_model = self.forecaster_architecture()
+        final_model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
+        self.forecaster = final_model
+
+
+    def forecaster_architecture(self, X):
+        raise NotImplementedError("The forecaster is not implemented. You must provide an implementation for forecaster_architecture method")
 
 class LSTMCNNForecaster(DeepForecaster):
     """
     LSTM-CNN Forecaster Class
     """
-    def __init__(self, window_size, steps_ahead, n_series):
-        super().__init__()
-        self.steps_ahead = steps_ahead
+    def __init__(self, n_series, lstm_hiddens=256, dropout_rate=0.9, lstm_activation='tanh', conv_activation='relu', gaussian_noise=0.01, *args, **kwargs):
+        self.lstm_hiddens = lstm_hiddens
+        self.dropout_rate = dropout_rate
+        self.lstm_activation = lstm_activation
+        self.conv_activation = conv_activation
+        self.gaussian_noise = gaussian_noise
         self.n_series = n_series
-        self.window_size = window_size
-        self.construct_forecaster()
-        self.fit = self.forecaster.fit
-        self.predict = self.forecaster.predict
-
-    def construct_forecaster(self):
+        super().__init__(*args, **kwargs)
+        
+    def forecaster_architecture(self):
         input_model = tf.keras.Input(shape=(self.window_size, self.n_series))
-        x = tf.keras.layers.GaussianNoise(0.01)(input_model)
-        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, activation='tanh', use_bias=True))(x)
+        x = tf.keras.layers.GaussianNoise(self.gaussian_noise)(input_model)
+        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.lstm_hiddens, activation=self.lstm_activation, use_bias=True))(x)
         x = tf.keras.layers.RepeatVector(14)(x)
-        x = tf.keras.layers.Dropout(0.9)(x)
-        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, activation='tanh', return_sequences=False, use_bias=True))(x)
+        x = tf.keras.layers.Dropout(self.dropout_rate)(x)
+        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.lstm_hiddens, activation=self.lstm_activation, return_sequences=False, use_bias=True))(x)
         x = tf.keras.layers.RepeatVector(self.window_size)(x)
-        x = tf.keras.layers.Dropout(0.9)(x)
+        x = tf.keras.layers.Dropout(self.dropout_rate)(x)
         x = tf.keras.layers.Concatenate(axis=2)([input_model, x])
-        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, activation='tanh', return_sequences=False, use_bias=True))(x)
+        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.lstm_hiddens, activation=self.lstm_activation, return_sequences=False, use_bias=True))(x)
         x = tf.keras.layers.RepeatVector(self.steps_ahead)(x)
-        x = tf.keras.layers.Dropout(0.9)(x)
-        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, activation='tanh', return_sequences=True, use_bias=True))(x)
-        x = tf.keras.layers.Dropout(0.4)(x)
-        x = tf.keras.layers.Conv1D(64, 1, 1, activation='relu', use_bias=True)(x)
-
-        output_model = tf.keras.layers.Conv1D(self.n_series, 1, 1, activation='relu', use_bias=True)(x)
-
-        final_model = tf.keras.Model(input_model, output_model)
-        final_model.compile(optimizer='adam', loss='mae', metrics=['mse',tf.keras.metrics.RootMeanSquaredError(),
-                                                    'mae', 'msle', tf.keras.losses.mean_absolute_percentage_error,
-                                                    tf.keras.losses.huber, tf.keras.losses.log_cosh])
-        self.forecaster = final_model
-
+        x = tf.keras.layers.Dropout(self.dropout_rate)(x)
+        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, activation=self.lstm_activation, return_sequences=True, use_bias=True))(x)
+        x = tf.keras.layers.Dropout(self.dropout_rate)(x)
+        x = tf.keras.layers.Conv1D(64, 1, 1, activation=self.conv_activation, use_bias=True)(x)
+        output = tf.keras.layers.Conv1D(self.n_series, 1, 1, activation=self.conv_activation, use_bias=True)(x)
+        
+        return tf.keras.Model(input_model, output)
+        
--- tests/test_forecasting.py
+++ tests/test_forecasting.py
@@ -4,7 +4,6 @@ import numpy as np
 X = np.random.normal(size=(100, 8, 10))
 y = np.random.normal(size=(100, 4, 10))
 
-model = LSTMCNNForecaster(8, 4, 10)
-model.construct_forecaster()
-model.fit(X, y)
+model = LSTMCNNForecaster(window_size=8, steps_ahead=4, n_series=10)
+model.fit(X, y, epochs=10)
 print(model.predict(X).shape)
