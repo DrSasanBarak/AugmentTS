af4183ec0b9a88ab3e278e5dd9c3635e7fdaae0d
Amirabbas Asadi <amir137825@gmail.com>
2021-11-24 14:00:19 +0330

    change pakcage name in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="taug",
+    name="augmentts",
     version="0.1.0",
     description="Time Series Forecasting and Data Augmentation using Deep Generative Models ",
     long_description=README,
